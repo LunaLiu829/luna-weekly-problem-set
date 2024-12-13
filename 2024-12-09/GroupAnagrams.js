a491b57142bb2cbc17b2825bfc51f109cc9ebed9
LunaLiu4 <luna.liu829@gmail.com>
2024-12-13 15:49:02 -0600

    groupAnagrams

---
+++ 2024-12-09/GroupAnagrams.js
@@ -0,0 +1,31 @@
+// Group Anagrams
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+// Example 1:
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// Example 2:
+// Input: strs = [""]
+// Output: [[""]]
+// Example 3:
+// Input: strs = ["a"]
+// Output: [["a"]]
+const groupAngrams = (strs) => {
+  let result = new Map();
+  for (let str of strs) {
+    //sort the string by each character
+    let newString = str.split("").sort().join("");
+    // array.push(str);
+    if (!result.has(newString)) {
+      result.set(newString, Array.isArray(str) ? str : [str]);
+    } else {
+      let currentValue = result.get(newString);
+      currentValue.push(...[str]);
+      result.set(newString, currentValue);
+    }
+  }
+  const valuesArray = [...result.values()];
+  return valuesArray;
+};
+console.log(groupAngrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+console.log(groupAngrams([""]));
+console.log(groupAngrams(["a"]));
